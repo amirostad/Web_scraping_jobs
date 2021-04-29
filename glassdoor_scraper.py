cdda350f99cf6008fcd617f18414ed2551e38c53
Amir Ostad <amirmasood.ostad@gmail.com>
2021-04-28 22:37:00 -0400

    added code to get rid of signup prompt on glassdoor.

--- glassdoor_scraper.py
+++ glassdoor_scraper.py
@@ -15,7 +15,7 @@ import time
 
 DRIVER_PATH = 'chromedriver'
 options = Options()
-options.headless = False
+options.headless = False # With (FALSE) or without (TRUE) user interface
 options.add_argument("--window-size=1200,675")
 
 PATIENCE_TIME = 60
@@ -25,8 +25,18 @@ job_to_lookfor = 'Data%20Scientist'
 url = "https://www.glassdoor.com/Job/jobs.htm?sc.keyword=" + job_to_lookfor + "&suggestCount=0&suggestChosen=false&clickSource=searchBox&locId=1&locT=N&locName=United%20States"
 driver.get(url)
 
-page_number = 1 # currently on page 1
+page_number = 1 # Currently on page 1
 while (page_number <= 5):
+    
+    # Test for the "Sign Up" prompt and get rid of it.
+    try:
+        driver.find_element_by_css_selector('[alt="Close"]').click() # Clicking to the X.
+        print(' x out worked')
+    except NoSuchElementException:
+        print(' x out failed')
+        pass
+    
+    # Go page after page and scrape jobs
     try:
         print('currently on page {}'.format(page_number))
         nextPageButton = driver.find_element_by_xpath('.//li[@class="css-1yshuyv e1gri00l3"]//a')
