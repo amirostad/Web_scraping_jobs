deafb63c4f08b1ac0d1811b3583edf9311d79574
Amir Ostad <amirmasood.ostad@gmail.com>
2021-05-12 01:04:24 -0400

    added a function to data_cleaning.py and gave credits to the authors.

---
+++ modules/data_cleaning.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 11 21:00:07 2021
+
+@author: Amir Ostad 
+"""
+import glassdoor_scraper as gs
+import pandas as pd
+
+def get_clean_dataframe(num_jobs=100):
+    
+    df = gs.get_jobs(num_jobs=num_jobs)
+    
+    # df = pd.read_csv('/data/df.csv')
+    
+    #%% Salary
+    
+    # Remove instances without salary estimates
+    df = df[df['Salary Estimate'] != '-1']
+    
+    ## Remove K and $ 
+    df['Salary Estimate'] = df['Salary Estimate'].apply(lambda x: x.replace('K', '').replace('$', ''))
+    
+    ## Add hourly and employer provided binary columns
+    df['Hourly'] = df['Salary Estimate'].apply(lambda x: 1 if 'per hour' in x.lower() else 0)
+    df['Employer_provided'] = df['Salary Estimate'].apply(lambda x: 1 if 'employer est' in x.lower() else 0)
+    
+    ## Add min, max, and average salary columns and cast them into integer type
+    df['Min_salary'] = df['Salary Estimate'].apply(lambda x: int(x.split()[0]))
+    df['Max_salary'] = df['Salary Estimate'].apply(lambda x: int(x.split()[2]))
+    df['Average_salary'] = (df.Min_salary + df.Max_salary)/2
+    
+    ## Remove the initial salary estimate column
+    df.drop('Salary Estimate', axis=1, inplace=True)
+    
+    #%% Rating
+    
+    ## Ensure ratings are float type
+    df['Rating'] = df['Rating'].apply(lambda x: float(x))
+    
+    #%% Company name
+    
+    ## Remove rating from the end of company names
+    df['Company Name'] = df['Company Name'].apply(lambda x: x.split('\n')[0])
+    
+    #%% State: what state is the job in
+    
+    df['State'] = df['Location'].apply(lambda x: x.split()[-1])
+    
+    #%% Company age
+    
+    # Replace non-numeric cells in Founded column with -1
+    df['Founded'].replace('[^0-9]', -1, regex=True, inplace=True)
+    
+    df['Company_age'] = df['Founded'].apply(lambda x: 2021-int(x) if int(x)>0 else -1)
+    
+    
+    #%% Drop unwanted columns, reorder the remining columns, and write the dataframe to csv format
+    
+    df.drop(['Company Name', 'Location', 'Size', 'Founded', 'Job Title', 
+             'Type of ownership', 'Industry', 'Sector', 'Revenue'], inplace=True,
+            axis=1)
+    
+    df = df.reindex(columns=['Average_salary', 'Rating', 'Hourly', 
+                             'Employer_provided', 'Min_salary',
+                             'Max_salary', 'Company_age', 'State'])
+    
+    df.to_csv('df_cleaned.csv', index=False)
+    
+    return df
+    
--- modules/glassdoor_scraper.py
+++ modules/glassdoor_scraper.py
@@ -2,7 +2,14 @@
 """
 Created on Mon Apr 26 20:18:20 2021
 
-@author: Amir Ostad
+author: Kenarapfaik
+url: https://github.com/PlayingNumbers/ds_salary_proj/blob/master/glassdoor_scraper.py
+
+revision and edit: Amir Ostad
+
+The original code from Kenarapfaik had redundancies and broken selenium
+code. I redid several parts of the code and fixed the xpaths.
+
 """
 
 from selenium import webdriver
@@ -15,45 +22,6 @@ import time
 import pandas as pd
 import snoop
 
-# DRIVER_PATH = 'chromedriver'
-# options = Options()
-# options.headless = False # With (FALSE) or without (TRUE) user interface
-# options.add_argument("--window-size=1200,675")
-
-# PATIENCE_TIME = 60
-
-# driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
-# job_to_lookfor = 'Data%20Scientist'
-# url = "https://www.glassdoor.com/Job/jobs.htm?sc.keyword=" + job_to_lookfor + "&suggestCount=0&suggestChosen=false&clickSource=searchBox&locId=1&locT=N&locName=United%20States"
-# driver.get(url)
-
-# page_number = 1 # Currently on page 1
-# while (page_number <= 5):
-    
-#     # Test for the "Sign Up" prompt and get rid of it.
-#     try:
-#         driver.find_element_by_css_selector('[alt="Close"]').click() # Clicking to the X.
-#         print(' x out worked')
-#     except NoSuchElementException:
-#         print(' x out failed')
-#         pass
-    
-#     # Go page after page and scrape jobs
-#     try:
-#         print('currently on page {}'.format(page_number))
-#         nextPageButton = driver.find_element_by_xpath('.//li[@class="css-1yshuyv e1gri00l3"]//a')
-#         time.sleep(2)
-#         nextPageButton.click()
-#         page_number += 1
-#         print('going to page {}'.format(page_number))
-#         time.sleep(5)
-#     except Exception as e:
-#         print(e)
-#         break
-# print("********** Next page process complete! **********")
-# time.sleep(10)
-
-##############################################################################
 @snoop # snoop shows the line of a function being executed
 def get_jobs(job_to_lookfor='Data%20Scientist', num_jobs=10, verbose=False, DRIVER_PATH = 'chromedriver', slp_time=3):
     
