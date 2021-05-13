fed8dd3640353ffbb8877d5fb8d306932bff4399
Amir Ostad <amirmasood.ostad@gmail.com>
2021-05-12 21:41:29 -0400

    Finished revising the code.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import eda
 import joblib
 import model
 import pandas as pd
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 #%% Get the clean data
 df = dc.get_clean_dataframe()
@@ -26,11 +26,15 @@ eda.get_countplot(df)
 try:
     # load the model form disk
     loaded_model = joblib.load("saved_model.sav")
-    print("The optimized model: \n", loaded_model)
     
 except:
     loaded_model = model.get_model(df)
-   
+    
+print("The optimized model: \n", loaded_model)
+print("The number of features used to fit the model: {}". format(loaded_model.n_features_))    
+
+#%% Model's accuracy
+
 X_test = pd.read_csv('./data/X_test.csv')
 y_test = pd.read_csv('./data/y_test.csv')
 y_pred = loaded_model.predict(X_test)    
@@ -40,4 +44,5 @@ feat_importances = pd.Series(loaded_model.feature_importances_,
                                  index=X_test.columns)
 feat_importances.nlargest(20).plot(kind='barh', color='teal')
 print("Model's mean absolute error (MAE) = ", mean_absolute_error(y_test, y_pred))
-print("The number of features used to fit the model: {}". format(loaded_model.n_features_))
+print("Model's mean squared error (MSE) = ", mean_squared_error(y_test, y_pred))
+print("Model's R2 score = ", r2_score(y_test, y_pred))
